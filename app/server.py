1fe4d8029b2bf068607bc3f42fa43907674ab834
aleros-web <54913953+aleros-web@users.noreply.github.com>
2019-09-20 21:26:45 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/01lbprvk7oeo7vs/export_v3.pkl?dl=1'
-export_file_name = 'export_v3.pkl'
+export_file_url = 'https://www.dropbox.com/s/l7v9xds2icebq2l/export_V4.pkl?dl=1'
+export_file_name = 'export_v4.pkl'
 
 classes = ['superbike', 'naked', 'offroad', 'cruiser', 'touring']
 path = Path(__file__).parent
